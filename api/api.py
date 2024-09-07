8898ecf19fa7fa8ca2f2983b7fa41b8aa7d8e8c5
karunyapenumalla <karunya.penumalla@gmail.com>
2024-09-07 11:11:03 -0700

    added asteroid api support

--- api/api.py
+++ api/api.py
@@ -15,6 +15,14 @@ def index():
 @app.route('/api/time')
 def get_current_time():
     return {'time' : time.time()}
+import requests
+
+def get_asteroids(**kwargs):
+    url = "https://api.nasa.gov/neo/rest/v1/feed?"
+    for key, value in kwargs.items():
+        url += f"{key}={value}&"
+    response = requests.get(url)
+    return response.json()
 
 if __name__ == "__main__":
     serve(app, host = '127.0.0.1', port = 8085)
