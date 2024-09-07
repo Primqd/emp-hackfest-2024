3abc381e73f24db5f289eb369d25d81bb56e1f1e
Franklin Chen <franklinvchen@outlook.com>
2024-09-07 10:51:04 -0700

    added waitress for localhost

--- api/api.py
+++ api/api.py
@@ -1,8 +1,20 @@
 import time
 from flask import Flask
+from waitress import serve
+import logging
 
-app = Flask(__name__)
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s %(message)s')
 
-@app.route('/time')
+app = Flask(__name__, static_folder='../build', static_url_path="/")
+
+@app.route('/') # simulate default filename when ends with /
+def index():
+    # return "hello world"
+    return app.send_static_file('index.html')
+
+@app.route('/api/time')
 def get_current_time():
     return {'time' : time.time()}
+
+if __name__ == "__main__":
+    serve(app, host = '127.0.0.1', port = 8085)
